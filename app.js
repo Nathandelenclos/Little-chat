d7844563d4a09af513e60057df5118bc3f0b0b20
Nathan <nathan.delenclos@epitech.eu>
2022-02-03 16:52:05 +0100

    [+] correcteur

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function message(data) {
-        ws.emit('message', data);
-        console.log("%s", data);
+        wss.clients.forEach(function each(client) {
+            client.send(data);
+        });
     });
 });
