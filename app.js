9028c067b43606f852d4f90fb1bf66c22090d23d
Nathan <nathan.delenclos@epitech.eu>
2022-02-03 17:03:56 +0100

    [+] correcteur

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const { WebSocketServer } = require('ws');
 
 const wss = new WebSocketServer({ port: 3000 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(wsn, req) {
     ws.on('message', function message(data) {
         wss.clients.forEach(function each(client) {
             client.send(data);
         });
+    });
+    wss.clients.forEach(function each(client) {
+        client.send("New connection => " + req.socket.remoteAddress);
     });
 });
